42b8952a435cee94679959109f75e2b612cf6320
Bhanu <bhanupratap.negi@hugohub.com>
2026-03-21 22:55:10 +0530

    REPL Initialized

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 /*
 Making REPL
-Reach Execute Process Loop
+Read Execute Process Loop
 */
 
 class InputBuffer {
 public:
     char* buffer;
     size_t buffer_length;
-    size_t input_length;
+    ssize_t input_length;
 
     // Not making the input buffer as static instance, because we may need to send different requests from different threads
     InputBuffer() {
@@ -19,8 +19,18 @@ public:
         this->input_length = 0;
     }
 
-    void print_command() {
-    
+    void print_prompt() { cout<<"BpsnDB >> "; }
+
+    void read_input() {
+        //Fun fact: ssize_t is a fancy way to represent long
+        ssize_t bytes_read = getline(&(this->buffer), &(this->buffer_length), stdin);
+        if (bytes_read <= 0) {
+            cout<<"Error reading input\n";
+            exit(EXIT_FAILURE);
+        }
+        // Ignore trailing newline
+        this->input_length = bytes_read - 1;
+        this->buffer[bytes_read - 1] = 0;
     }
 };
 
@@ -29,10 +39,21 @@ int main(int argc, char *argv[]) {
     InputBuffer* inputBuffer = new InputBuffer();
     // Continously reading the db input
     while(1) {
+        inputBuffer->print_prompt();
+        inputBuffer->read_input();
 
+        if (strcmp(inputBuffer->buffer, ".exit") == 0) {
+            free(inputBuffer);
+            exit(EXIT_SUCCESS);
+        } else if (!inputBuffer->input_length) {
+            // Foe handling if when the user enters a new line only. i.e. just enter
+            continue;
+        } else {
+            printf("Unrecognized command '%s'.\n", inputBuffer->buffer);
+        }
     }
 
     // To tell the shell that the program executed correctly, or else it would be anything other than 0.
-    // Weird right?
+    // Weird right? => How many things went wrong = 0
     return 0;
 }
