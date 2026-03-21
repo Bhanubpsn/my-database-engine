50a439a9240552dc122fe8a8b0c19252200e66e5
Bhanu <bhanupratap.negi@hugohub.com>
2026-03-22 00:14:26 +0530

    Parser initialised

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "types.h"
 using namespace std;
 
 /*
@@ -30,28 +31,83 @@ public:
         }
         // Ignore trailing newline
         this->input_length = bytes_read - 1;
+        // Input End representation 0
         this->buffer[bytes_read - 1] = 0;
     }
+
+    // Parser for META commands
+    MetaCommandResult do_meta_command() {
+        if (strcmp(this->buffer, ".exit") == 0) {
+            exit(EXIT_SUCCESS);
+        } else {
+            return META_COMMAND_UNRECOGNIZED_COMMAND;
+        }
+    }
 };
+
+class Statement {
+public:
+    StatementType type;
+
+    void execute_statement() {
+        switch (this->type) {
+            case (STATEMENT_INSERT):
+                cout<<"This is where we would do an insert.\n";
+                break;
+            case (STATEMENT_SELECT):
+                cout<<"This is where we would do a select.\n";
+                break;
+        }
+    }
+
+    // Parser for SQL commands
+    PrepareResult prepare_statement(char* input_buffer) {
+        if (strncasecmp(input_buffer, "insert", 6) == 0) {
+            this->type = STATEMENT_INSERT;
+            return PREPARE_SUCCESS;
+        }
+        if (strcasecmp(input_buffer, "select") == 0) {
+            this->type = STATEMENT_SELECT;
+            return PREPARE_SUCCESS;
+        }
+        return PREPARE_UNRECOGNIZED_STATEMENT;
+    }
+};  
 
 // the input arguements are always argc = x + 1, 1 becuase of the run command of the cpp/c file and then the input arguement array
 int main(int argc, char *argv[]) {
     InputBuffer* inputBuffer = new InputBuffer();
+    Statement* statement = new Statement();
     // Continously reading the db input
     while(1) {
         inputBuffer->print_prompt();
         inputBuffer->read_input();
 
-        if (strcmp(inputBuffer->buffer, ".exit") == 0) {
-            free(inputBuffer);
-            exit(EXIT_SUCCESS);
-        } else if (!inputBuffer->input_length) {
-            // Foe handling if when the user enters a new line only. i.e. just enter
-            continue;
-        } else {
-            printf("Unrecognized command '%s'.\n", inputBuffer->buffer);
+        if (inputBuffer->buffer[0] == '.') {
+            switch (inputBuffer->do_meta_command()) {
+                case (META_COMMAND_SUCCESS):
+                    continue;
+                case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                    printf("Unrecognized command '%s'\n", inputBuffer->buffer);
+                continue;
+            }
         }
+
+        switch (statement->prepare_statement(inputBuffer->buffer)) {
+            case (PREPARE_SUCCESS):
+                break;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                printf("Unrecognized keyword at start of '%s'.\n",inputBuffer->buffer);
+                continue;
+        }
+
+        statement->execute_statement();
+        cout<<"Executed.\n";
     }
+
+    // Free up the heap
+    delete statement;
+    delete inputBuffer;
 
     // To tell the shell that the program executed correctly, or else it would be anything other than 0.
     // Weird right? => How many things went wrong = 0
