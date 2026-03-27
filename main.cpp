dc0ef28eea0dbcfe793293f4776e6117e50a21ca
Bhanu <bhanupratap.negi@hugohub.com>
2026-03-27 13:41:01 +0530

    Added update statement

--- main.cpp
+++ main.cpp
@@ -52,6 +52,34 @@ public:
         return EXECUTE_SUCCESS;
     }
 
+    ExecuteResult execute_update(Table* table) {
+        uint32_t key_to_update = row_to_insert.id;
+        Cursor* cursor = new Cursor();
+        
+        // Finding where the key should be
+        cursor->table_find(table, key_to_update);
+
+        // Accessing the specific page the cursor found
+        void* node = table->pager->get_page(cursor->page_num);
+        LeafNode leafNode;
+        leafNode.node = (uint8_t*)node;
+
+        uint32_t num_cells = *leafNode.leaf_node_num_cells();
+
+        // Verifying the key actually exists
+        if (cursor->cell_num < num_cells) {
+            uint32_t key_at_index = *leafNode.leaf_node_key(cursor->cell_num);
+            if (key_at_index == key_to_update) {
+                // Overwriting the existing row data
+                row_to_insert.serialize_row(leafNode.leaf_node_value(cursor->cell_num));
+                return EXECUTE_SUCCESS;
+            }
+        }
+
+        delete cursor;
+        return EXECUTE_KEY_NOT_FOUND;
+    }
+
     // Printing every row in the table
     ExecuteResult execute_select(Table* table) {
         Cursor* cursor = new Cursor();
@@ -76,6 +104,8 @@ public:
                 return execute_insert(table);
             case (STATEMENT_SELECT):
                 return execute_select(table);
+            case (STATEMENT_UPDATE):
+                return execute_update(table);
         }
     }
 
@@ -109,11 +139,44 @@ public:
         return PREPARE_SUCCESS;
     }
 
+    PrepareResult prepare_update(char* input_buffer) {
+        this->type = STATEMENT_UPDATE;
+
+        char* keyword = strtok(input_buffer, " ");
+        char* id_string = strtok(nullptr, " ");
+        char* username = strtok(nullptr, " ");
+        char* email = strtok(nullptr, " ");
+
+        if (id_string == nullptr || username == nullptr || email == nullptr) {
+            return PREPARE_SYNTAX_ERROR;
+        }
+
+        int id = atoi(id_string);
+        if (id < 0) {
+            return PREPARE_NEGATIVE_ID;
+        }
+        if (strlen(username) > COLUMN_USERNAME_SIZE) {
+            return PREPARE_STRING_TOO_LONG;
+        }
+        if (strlen(email) > COLUMN_EMAIL_SIZE) {
+            return PREPARE_STRING_TOO_LONG;
+        }
+
+        this->row_to_insert.id = id;
+        strcpy(this->row_to_insert.username, username);
+        strcpy(this->row_to_insert.email, email);
+
+        return PREPARE_SUCCESS;
+    }
+
 
     // Parser for SQL commands
     PrepareResult prepare_statement(char* input_buffer) {
         if (strncasecmp(input_buffer, "insert", 6) == 0) {
             return prepare_insert(input_buffer);
+        }
+        if (strncasecmp(input_buffer, "update", 6) == 0) {
+            return prepare_update(input_buffer);
         }
         if (strcasecmp(input_buffer, "select") == 0) {
             this->type = STATEMENT_SELECT;
@@ -177,6 +240,9 @@ int main(int argc, char *argv[]) {
                 break;
             case (EXECUTE_TABLE_FULL):
                 printf("Error: Table full.\n");
+                break;
+            case (EXECUTE_KEY_NOT_FOUND):
+                printf("Can't find the key to update.\n");
                 break;
         }
     }
